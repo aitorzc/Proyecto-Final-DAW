472029cc457ca05fc9472901bfdce77646c03cc3
Aitor Zuñiga Canovas <Aitor Zuiga Canovas>
2017-03-08 15:01:00 +0100

    Nuevo archivo de registro html + archivo registro.css y js

--- view/main.js
+++ view/main.js
@@ -1,12 +1,19 @@
+var btnMenu = document.getElementById("btnMenu");
+var sidenav = document.getElementById("sidenav");
 //Abrir el nav lateral
 function openSidenav(){
 
-    document.getElementById("sidenav").style.width = "12vw";
-
+    sidenav.style.width = "12vw";
+    btnMenu.firstChild.className = "glyphicon glyphicon-menu-left";
+    btnMenu.removeAttribute("onclick");
+    btnMenu.setAttribute("onclick", "closeSidenav()");
 }
 //Cerrar el nav lateral
 function closeSidenav(){
 
-    document.getElementById("sidenav").style.width = "0";
+    sidenav.style.width = "0";
+    btnMenu.firstChild.className = "glyphicon glyphicon-menu-hamburger";
+    btnMenu.removeAttribute("onclick");
+    btnMenu.setAttribute("onclick",  "openSidenav()");
 
 }
---
+++ view/register.js
@@ -0,0 +1,12 @@
+function formValidate(){
+    var nick = document.getElementById("nick");
+    var email = document.getElementById("email");
+    var pswd = document.getElementById("pswd");
+    var pswd2 = document.getElementById("pswd2");
+    if(){
+
+
+
+    }
+
+}
